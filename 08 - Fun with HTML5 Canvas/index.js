cc6a985eee5dc3e18ec1a5e9c30701910575cdc4
Andrew Ploeger <andrewploeger@gmail.com>
2017-05-09 22:35:02 -0600

    day 8 activity

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>HTML5 Canvas</title>
-</head>
-<body>
-<canvas id="draw" width="800" height="800"></canvas>
-<script>
 const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
+const conteckst = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 100;
+conteckst.strokeStyle = '#BADA55';
+conteckst.lineJoin = 'round';
+conteckst.lineCap = 'round';
+conteckst.lineWidth = 100;
 // ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
@@ -26,27 +17,27 @@ let direction = true;
 function draw(e) {
   if (!isDrawing) return; // stop the fn from running when they are not moused down
   console.log(e);
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.beginPath();
+  conteckst.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  conteckst.beginPath();
   // start from
-  ctx.moveTo(lastX, lastY);
+  conteckst.moveTo(lastX, lastY);
   // go to
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+  conteckst.lineTo(e.offsetX, e.offsetY);
+  conteckst.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
 
   hue++;
   if (hue >= 360) {
     hue = 0;
   }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+  if (conteckst.lineWidth >= 100 || conteckst.lineWidth <= 1) {
     direction = !direction;
   }
 
   if(direction) {
-    ctx.lineWidth++;
+    conteckst.lineWidth++;
   } else {
-    ctx.lineWidth--;
+    conteckst.lineWidth--;
   }
 
 }
@@ -60,14 +51,3 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
-
-</script>
-
-<style>
-  html, body {
-    margin:0;
-  }
-</style>
-
-</body>
-</html>
